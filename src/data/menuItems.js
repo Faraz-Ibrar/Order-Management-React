2d946a74d3a60f5699946a9e897d969d0267d9c8
FARAZ IBRAR <farazibrar9697@gmail.com>
2025-05-03 12:17:01 +0500

    Debugging Images

--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -1,64 +1,66 @@
+// src/data/menuItems.js
+
 export const menuItems = [
   {
     id: '1',
     name: 'Butter Chicken',
-    image: '/images/Butter-Chicken.jpg',
+    image: `${process.env.PUBLIC_URL}/images/Butter-Chicken.jpg`,
     price: 1200,
     serves: 2
   },
   {
     id: '2',
     name: 'Vegetable Biryani',
-    image: '/images/Vegetable-Biryani.jpg',
+    image: `${process.env.PUBLIC_URL}/images/Vegetable-Biryani.jpg`,
     price: 800,
     serves: 2
   },
   {
     id: '3',
     name: 'Beef Karahi',
-    image: '/images/Beef-Karahi.jpg',
+    image: `${process.env.PUBLIC_URL}/images/Beef-Karahi.jpg`,
     price: 1500,
     serves: 3
   },
   {
     id: '4',
     name: 'Chapli Kebab',
-    image: '/images/Chapli-Kebab.jpg',
+    image: `${process.env.PUBLIC_URL}/images/Chapli-Kebab.jpg`,
     price: 600,
     serves: 1
   },
   {
     id: '5',
     name: 'Chicken Alfredo Pasta',
-    image: '/images/Pasta.jpeg',
+    image: `${process.env.PUBLIC_URL}/images/Pasta.jpeg`,
     price: 1000,
     serves: 2
   },
   {
     id: '6',
     name: 'Mutton Korma',
-    image: '/images/Mutton-Korma.jpg',
+    image: `${process.env.PUBLIC_URL}/images/Mutton-Korma.jpg`,
     price: 1400,
     serves: 2
   },
   {
     id: '7',
     name: 'Paneer Tikka',
-    image: '/images/Paneer-Tikka.jpg',
+    image: `${process.env.PUBLIC_URL}/images/Paneer-Tikka.jpg`,
     price: 900,
     serves: 2
   },
   {
     id: '8',
     name: 'Seekh Kebab',
-    image: '/images/Seekh-Kebab.jpg',
+    image: `${process.env.PUBLIC_URL}/images/Seekh-Kebab.jpg`,
     price: 700,
     serves: 1
   },
   {
     id: '9',
     name: 'Chicken Malai Boti',
-    image: '/images/Chicken-Malai-Boti.jpg',
+    image: `${process.env.PUBLIC_URL}/images/Chicken-Malai-Boti.jpg`,
     price: 1100,
     serves: 2
   },
